7a95f2db0ebf49b9383e6f90b3b13af9c2c1853b
Nachiket <nachiketgadekar98@gmail.com>
2023-12-11 15:14:52 +0530

    changed logic and appearance

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 
 root = tk.Tk()
 root.title("Pomodoro App")
-root.geometry("430x550")
+root.geometry("350x400")
 
 timer_running = False
 break_running = False
@@ -13,84 +13,98 @@ start_time = 0
 remaining_time = 0
 elapsed_time = 0
 
-focus_duration = 1500  # 25 minutes
-break_duration = 300  # 5 minutes
+focus_duration = 25
+break_duration = 5
 
 pygame.mixer.init()
 timer_sound = r"C:\Users\Nachiket Gadekar\Documents\pomodoro project\Temple Bell-Sound.wav"
 
 
-def start_timer():
-    global timer_running, start_time, break_running
-    break_running = False
-    if not timer_running:
-        timer_running = True
-        start_time = time.time()
+class Timer:
+    def __init__(self, focus_duration, break_duration, root, timer_label, start_button, stop_button, start_break_button):
+        self.focus_duration = focus_duration
+        self.break_duration = break_duration
+        self.root = root
+        self.timer_label = timer_label
+        self.start_button = start_button
+        self.stop_button = stop_button
+        self.start_break_button = start_break_button
+        self.timer_running = False
+        self.break_running = False
+        self.start_time = 0
+        self.elapsed_time = 0
+        self.remaining_time = 0
 
-        update_timer()
-        start_button.config(state=tk.DISABLED)
-        start_break_button.config(state=tk.DISABLED)
-        stop_button.config(state=tk.NORMAL)
+    def start_timer(self):
+        self.break_running = False
+        if not self.timer_running:
+            self.timer_running = True
+            self.start_time = time.time()
+            self.update_timer()
+            self.start_button.config(state=tk.DISABLED)
+            self.start_break_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.NORMAL)
 
+    def stop_timer(self):
+        self.timer_running = False
+        self.start_button.config(state=tk.NORMAL)
+        self.start_break_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
 
-def stop_timer():
-    global timer_running, start_time
-    timer_running = not timer_running
-    if timer_running == True:
-        start_time = time.time() - elapsed_time
+    def start_break(self):
+        self.timer_running = False
+        if not self.break_running:
+            self.break_running = True
+            self.start_time = time.time()
+            self.update_timer()
+            self.start_break_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.DISABLED)
+            self.start_button.config(state=tk.NORMAL)
 
-        update_timer()
-        start_break_button.config(state=tk.DISABLED)
-        stop_button.config(state=tk.NORMAL)
-    else:
-        start_button.config(state=tk.NORMAL)
-        start_break_button.config(state=tk.NORMAL)
+    def update_timer(self):
+        if self.timer_running or self.break_running:
+            self.elapsed_time = int(time.time() - self.start_time)
+            if self.timer_running and not self.break_running:
+                self.remaining_time = self.focus_duration - self.elapsed_time
+                if self.remaining_time <= -1:
+                    pygame.mixer.music.load(timer_sound)
+                    pygame.mixer.music.play()
+                    self.stop_timer()
+                    return
+            elif self.break_running and not self.timer_running:
+                self.remaining_time = self.break_duration - self.elapsed_time
+                if self.remaining_time <= -1:
+                    pygame.mixer.music.load(timer_sound)
+                    pygame.mixer.music.play()
+                    self.stop_timer()
+                    return
+            mins, secs = divmod(self.remaining_time, 60)
+            self.timer_label.config(text=f"{mins:02d}:{secs:02d}")
+            # Schedule next update after 500 milliseconds
+            self.root.after(500, self.update_timer)
 
-
-def start_break():
-    global break_running, start_time, break_duration, remaining_time, timer_running
-    timer_running = False
-    if not break_running:
-        break_running = True
-        start_time = time.time()
-
-        update_timer()
-        start_break_button.config(state=tk.DISABLED)
-        stop_button.config(state=tk.DISABLED)
-        start_button.config(state=tk.NORMAL)
-
-
-def update_timer():
-    global timer_running, start_time, break_running, elapsed_time, remaining_time
-    if timer_running or break_running:
-        elapsed_time = int(time.time() - start_time)
-        if timer_running == True and break_running == False:
-            remaining_time = focus_duration - elapsed_time
-            if remaining_time <= -1:
-                pygame.mixer.music.load(timer_sound)
-                pygame.mixer.music.play()
-                stop_timer()
-                return
-        elif break_running == True and timer_running == False:
-            remaining_time = break_duration - elapsed_time
-            if remaining_time <= -1:
-                pygame.mixer.music.load(timer_sound)
-                pygame.mixer.music.play()
-                stop_timer()
-                return
-        mins, secs = divmod(remaining_time, 60)
-        timer_label.config(text=f"{mins:02d}:{secs:02d}")
-
-        # Schedule next update after 1000 milliseconds
-        root.after(1000, update_timer)
+    def update_focus_duration(self, new_focus_duration):
+        # Update attribute
+        self.focus_duration = new_focus_duration
+        self.timer_running = False
+        self.break_running = False
+        self.start_time = 0
+        self.elapsed_time = 0
+        self.remaining_time = 0
+        # Update timer label
+        mins = self.focus_duration // 60
+        secs = self.focus_duration % 60
+        self.timer_label.config(text=f"{mins:02d}:{secs:02d}")
+        self.start_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
+        self.start_break_button.config(state=tk.DISABLED)
 
 
 def open_settings():
     settings_window = tk.Toplevel(root)
     settings_window.title("Settings")
     settings_window.geometry("300x200")
-
-    focus_label = tk.Label(settings_window, text="Focus Duration (seconds):")
+    focus_label = tk.Label(settings_window, text="Focus Duration (in minutes):")
     focus_label.pack()
     focus_entry = tk.Entry(settings_window)
     focus_entry.pack()
@@ -100,20 +114,17 @@ def open_settings():
 
 def save_settings(focus_duration_input):
     global focus_duration
-
     try:
         focus_duration = int(focus_duration_input)
-
     except ValueError:
         messagebox.showerror("Error", "Please enter valid numerical values for durations.")
         return
-
     if focus_duration <= 0:
         messagebox.showerror("Error", "Durations should be positive values.")
         return
+    timer.update_focus_duration(focus_duration*60)
 
 
-# add task to-do list
 def add_task():
     task = task_entry.get()
     if task:
@@ -128,19 +139,18 @@ def remove_selected_task(event):
         task_listbox.delete(selected_index)
 
 
-root.configure(bg="#DBF6FA")
-timer_label = tk.Label(root, bg="#D8F3F6", fg="black", bd=2, relief=tk.GROOVE, text="25:00", font=("Helvetica", 72))
-break_label = tk.Label(root, bg="#D8F3F6", fg="white", text="5:00", font=("Helvetica", 72))
-start_button = tk.Button(root, bg="#E66C41", fg="white", text="Start to focus", font=("Helvetica", 16),
-                         command=start_timer)
-stop_button = tk.Button(root, bg="#E66C41", fg="white", text="Pause", font=("Helvetica", 16), command=stop_timer)
-start_break_button = tk.Button(root, bg="#E66C41", fg="white", text="Start Break", font=("Helvetica", 16),
-                               command=start_break)
-settings_button = tk.Button(root, text="Settings", font=("Helvetica", 10), command=open_settings)
+root.configure(bg="#1a1625")
+timer_label = tk.Label(root, bg="#76737e", fg="white", bd=2, relief=tk.GROOVE, text="25:00", font=("Helvetica", 50))
+break_label = tk.Label(root, bg="#76737e", fg="white", text="5:00", font=("Helvetica", 50))
+start_button = tk.Button(root, bg="#7a5af5", text="Start to focus", font=("Helvetica", 12),command=lambda: timer.start_timer())
+stop_button = tk.Button(root, bg="#7a5af5", text="Pause", font=("Helvetica", 12), command=lambda: timer.stop_timer())
+start_break_button = tk.Button(root, bg="#7a5af5", text="Start Break", font=("Helvetica", 12),
+                               command=lambda: timer.start_break())
+settings_button = tk.Button(root, text="Settings", font=("Helvetica", 10),bg="#7a5af5", command=open_settings)
 
-task_entry = tk.Entry(root, font=("Helvetica", 16), width=30)
-add_button = tk.Button(root, bg="#E66C41", fg="white", text="Add Task", font=("Helvetica", 16), command=add_task)
-task_listbox = tk.Listbox(root, font=("Helvetica", 16), width=20, selectmode=tk.SINGLE)
+task_entry = tk.Entry(root, font=("Helvetica", 12), width=25,bg="#76737e")
+add_button = tk.Button(root, bg="#7a5af5", text="Add Task", font=("Helvetica", 12), command=add_task)
+task_listbox = tk.Listbox(root, font=("Helvetica", 16), width=20, selectmode=tk.SINGLE,bg="#76737e")
 # bind double click event to remove selected task
 task_listbox.bind("<Double-Button-1>", remove_selected_task)
 
@@ -155,5 +165,8 @@ start_break_button.pack(pady=5)
 task_entry.pack(pady=5)
 add_button.pack(pady=5)
 task_listbox.pack(pady=5)
+
+# timer object
+timer = Timer(focus_duration*60, break_duration*60, root, timer_label, start_button, stop_button, start_break_button)
 
 root.mainloop()
